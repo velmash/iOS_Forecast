3a4244eded2457e0c86aa00833bb16c5dfe15289
hyengchan <hyengchan@gmail.com>
2021-02-23 15:35:50 +0900

    current weather api (city id, coordinate)

--- iOS_Forecast/Api/CurrentWeatherApi.playground/Contents.swift
+++ iOS_Forecast/Api/CurrentWeatherApi.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import CoreLocation
 
 struct CurrentWeather: Codable {
     let dt: Int
@@ -21,42 +22,94 @@ struct CurrentWeather: Codable {
     let main: Main
 }
 
-func fetchCurrentWeather(cityName: String) {
-    let urlStr = "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=7fec3ee0ee44eae5296c1efbde974ad3&units=metric&lang=kr"
-    
+enum ApiError: Error {
+    case unknown
+    case invalidUrl(String)
+    case invalidResponse
+    case failed(Int)
+    case emplyData
+}
+
+func fetch<ParsingType: Codable>(urlStr: String, completion: @escaping (Result<ParsingType, Error>) -> ()) {
     guard let url = URL(string: urlStr) else {
-        fatalError("URL 생성 실패")
+        //fatalError("URL 생성 실패")
+        completion(.failure(ApiError.invalidUrl(urlStr)))
+        return
     }
     
     let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
         if let error = error {
-            fatalError("error")
+            //fatalError("error")
+            completion(.failure(error))
             return
         }
         
         guard let httpResponse = response as? HTTPURLResponse else {
-            fatalError("invalid response")
+            //fatalError("invalid response")
+            completion(.failure(ApiError.invalidResponse))
+            return
         }
         
         guard httpResponse.statusCode == 200 else {
-            fatalError("failed code \(httpResponse.statusCode)")
+            //fatalError("failed code \(httpResponse.statusCode)")
+            completion(.failure(ApiError.failed(httpResponse.statusCode)))
+            return
         }
         
         guard let data = data else {
-            fatalError("empty data")
+            //fatalError("empty data")
+            completion(.failure(ApiError.emplyData))
+            return
         }
         
         do {
             let decoder = JSONDecoder()
-            let weather = try decoder.decode(CurrentWeather.self, from: data)
+            let data = try decoder.decode(ParsingType.self, from: data)
             
-            print(weather.weather.first?.description)
-            print(weather.main.temp)
+            completion(.success(data))
         } catch {
-            fatalError(error.localizedDescription)
+            completion(.failure(error))
+            //fatalError(error.localizedDescription)
         }
     }
     task.resume()
 }
 
-fetchCurrentWeather(cityName: "seoul")
+func fetchCurrentWeather(cityName: String, completion: @escaping (Result<CurrentWeather, Error>) -> ()) {
+    let urlStr = "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=7fec3ee0ee44eae5296c1efbde974ad3&units=metric&lang=kr"
+    
+    fetch(urlStr: urlStr, completion: completion)
+}
+
+func fetchCurrentWeather(cityId: Int, completion: @escaping (Result<CurrentWeather, Error>) -> ()) {
+    let urlStr = "https://api.openweathermap.org/data/2.5/weather?id=\(cityId)&appid=7fec3ee0ee44eae5296c1efbde974ad3&units=metric&lang=kr"
+    
+    fetch(urlStr: urlStr, completion: completion)
+}
+
+func fetchCurrentWeather(location: CLLocation, completion: @escaping (Result<CurrentWeather, Error>) -> ()) {
+    let urlStr = "https://api.openweathermap.org/data/2.5/weather?lat=\(location.coordinate.latitude)&lon=\(location.coordinate.longitude)&appid=7fec3ee0ee44eae5296c1efbde974ad3&units=metric&lang=kr"
+    
+    fetch(urlStr: urlStr, completion: completion)
+}
+
+//fetchCurrentWeather(cityName: "seoul") {_ in }
+//
+//fetchCurrentWeather(cityId: 1835847) { (result) in
+//    switch result {
+//    case .success(let weather):
+//        dump(weather)
+//    case .failure(let error):
+//        print(error)
+//    }
+//}
+
+let location = CLLocation(latitude: 37.498206, longitude: 127.02761)
+fetchCurrentWeather(location: location) { (result) in
+    switch result {
+    case .success(let weather):
+        dump(weather)
+    case .failure(let error):
+        print(error)
+    }
+}
